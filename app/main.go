225c153cd93701d5a6fcc0265ba6d5ec4427cd7b
lgriffith10 <lgriffith.dev@gmail.com>
2026-01-14 17:02:51 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -8,28 +8,34 @@ import (
 )
 
 func main() {
-	fmt.Print("$ ")
-
 	reader := bufio.NewReader(os.Stdin)
-	command, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+
+	for {
+		fmt.Print("$ ")
+
+		command, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		command = strings.TrimSpace(command)
+
+		if command == "" {
+			continue
+		}
+
+		parts := strings.Fields(command)
+
+		instruction := parts[0]
+
+		switch instruction {
+		case "exit":
+			os.Exit(0)
+		case "echo":
+			fmt.Println(strings.Join(parts[1:], " "))
+		default:
+			fmt.Fprintf(os.Stderr, "%s: command not found\n", instruction)
+		}
 	}
-
-	command = strings.TrimSpace(command)
-	parts := strings.Split(command, " ")
-
-	instruction := parts[0]
-
-	switch instruction {
-	case "exit":
-		os.Exit(0)
-	case "echo":
-		fmt.Println(strings.Join(parts[1:], " "))
-	default:
-		fmt.Fprintf(os.Stderr, "%s: command not found\n", command)
-	}
-
-	main()
 }
